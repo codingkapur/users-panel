a2d206c09ff1ac52ab6c461f3df3f1b03daf5a0a
Raghav <codingkapur@gmail.com>
2021-10-26 14:18:15 +0530

    implement focus-on-toggle and register input from search input field

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,54 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { BsSearch, BsThreeDots } from "react-icons/bs";
 import { FiChevronRight, FiChevronLeft, FiChevronDown } from "react-icons/fi";
 import RowsContainer from "./RowsContainer";
 
 const Table = ({ userList, totalPages, handleClick, page }) => {
   const [searchState, setSearchState] = useState(false);
-  
+  const [searchInputText, setSearchInputText] = useState("");
+  const searchInput = useRef(null);
+
+  //   const focusOnClick = (searchState) => {
+  //     if (searchState) {
+  //       return
+  //     } else {
+  //       return searchInput.current.blur();
+  //     }
+  //   };
 
   const handleSearchIconClick = () => {
-      setSearchState(!searchState);
+    setSearchState(!searchState);
+    setSearchInputText("");
+  };
+
+  useEffect(() => {
+    focusOnClick();
+  }, [searchState]);
+
+  const focusOnClick = () => {
+    if (searchState) {
+      searchInput.current.focus();
+    } else {
+      searchInput.current.blur();
+    }
+  };
+
+  const handleSearchSubmit = (e) => {
+      e.preventDefault();
+      console.log(searchInputText);
   }
   return (
     <div className="table__container">
       <div className="table__top-row">
-        <div className="table__top-row--search-bar">
+        <form className="table__top-row--search-bar" onSubmit={(e)=> handleSearchSubmit(e)}>
           <BsSearch
             className="search__icon"
             onClick={() => handleSearchIconClick()}
           />
           <input
+            value={searchInputText}
+            onChange={(e) => setSearchInputText(e.target.value)}
+            ref={searchInput}
             type="text"
             className={
               searchState
@@ -27,7 +57,7 @@ const Table = ({ userList, totalPages, handleClick, page }) => {
             }
             placeholder="Search for a user"
           />
-        </div>
+        </form>
         <div className="table__toprow--empty-space"></div>
         <div className="table__top-row--operations">
           <button className="table__top-row--btn btn">View</button>
